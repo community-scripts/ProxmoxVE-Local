68541c0046671f6ae4535d66e03e6e79d6fd368a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-11-28 14:09:25 +0100

    Use tsx to run server.js - enables TypeScript imports for Prisma 7

--- src/server/db.js
+++ src/server/db.js
@@ -1,15 +1,6 @@
 import 'dotenv/config'
-import { createRequire } from 'module'
-import { fileURLToPath } from 'url'
-import { dirname, join } from 'path'
+import { PrismaClient } from '../../prisma/generated/prisma/client'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
-
-// Use createRequire with absolute path to .prisma/client/index.js
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-const require = createRequire(import.meta.url)
-const prismaClientPath = join(__dirname, '../../node_modules/.prisma/client/index.js')
-const { PrismaClient } = require(prismaClientPath)
 
 const globalForPrisma = globalThis;
 
