3a4f86942f8b76bb11acdf6b4463549da81026c5
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-11-28 13:30:17 +0100

    Remove circular import wrapper files (githubJsonService.js, localScripts.js)

--- src/server/services/githubJsonService.js
+++
@@ -1,6 +0,0 @@
-// JavaScript wrapper for githubJsonService.ts
-// This allows the JavaScript autoSyncService.js to import the TypeScript service
-
-import { githubJsonService } from './githubJsonService';
-
-export { githubJsonService };
